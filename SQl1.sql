3d5c4aa7257c940a22a4577f6a2e8a465a1ea29c
prema.v <prema.v>
2024-06-04 17:10:54 +0530

    hi

---
+++ SQl1.sql
@@ -0,0 +1,25 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ * Project :      DATA MODEL
+ *
+ * Date Created : Tuesday, June 04, 2024 17:10:54
+ * Target DBMS : Microsoft SQL Server 2019
+ */
+
+/* 
+ * TABLE: Entity1 
+ */
+
+CREATE TABLE Entity1(
+    A    char(10)    NULL
+)
+
+go
+
+
+IF OBJECT_ID('Entity1') IS NOT NULL
+    PRINT '<<< CREATED TABLE Entity1 >>>'
+ELSE
+    PRINT '<<< FAILED CREATING TABLE Entity1 >>>'
+go
+

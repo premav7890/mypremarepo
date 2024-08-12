fc4b54fe292f1b8cb141cbae9f315816587c0e13
premav7890 <premav7890>
2024-08-12 15:14:22 +0530

    hi

---
+++ Sn.sql
@@ -0,0 +1,19 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      DATA MODEL
+--
+-- Date Created : Monday, August 12, 2024 15:14:22
+-- Target DBMS : Snowflake
+--
+
+-- 
+-- TABLE: "Entity1" 
+--
+
+CREATE TABLE "Entity1"(
+    "A"  CHAR(10)
+)
+;
+
+
+

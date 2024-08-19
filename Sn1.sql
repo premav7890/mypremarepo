50029ff61e7887fc924c26608cc08292ac778a30
premav7890 <premav7890>
2024-08-19 20:54:06 +0530

    hi

---
+++ Sn1.sql
@@ -0,0 +1,22 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      31508.DM1
+--
+-- Date Created : Monday, August 19, 2024 20:54:06
+-- Target DBMS : Snowflake
+--
+
+-- 
+-- TABLE: "Entity1" 
+--
+
+CREATE TABLE "Entity1"(
+    "col2"  CHAR(10)    NOT NULL,
+    "col1"  CHAR(10),
+    "col3"  CHAR(10),
+    CONSTRAINT "PK1" PRIMARY KEY ("col2") NOT DEFERRABLE 
+)
+;
+
+
+

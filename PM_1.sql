5ec320018031c32e9b434ec3c51e8e40cd19cfe4
premav7890 <premav7890>
2024-08-20 16:02:39 +0530

    hi

---
+++ PM_1.sql
@@ -0,0 +1,20 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      TestDiagram.DM1
+--
+-- Date Created : Tuesday, August 20, 2024 16:02:39
+-- Target DBMS : PostgreSQL 10.x-12.x
+--
+
+-- 
+-- TABLE: "Entity1" 
+--
+
+CREATE TABLE "Entity1"(
+    "attribute1"  time(6) WITH TIME ZONE,
+    "attribute2"  time(6) WITH TIME ZONE
+)
+;
+
+
+

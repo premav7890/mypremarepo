2655a4270d29dc827b20621f4025600b1ed035d9
premav7890 <premav7890>
2024-09-19 11:36:35 +0530

    hi git

---
+++ Sntarget.sql
@@ -0,0 +1,21 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      DATA MODEL
+--
+-- Date Created : Thursday, September 19, 2024 11:36:35
+-- Target DBMS : Snowflake
+--
+
+-- 
+-- TABLE: "Entity1" 
+--
+
+CREATE TABLE "Entity1"(
+    "B"      CHAR(10)    NOT NULL,
+    "test1"  CHAR(10),
+    PRIMARY KEY ("B") NOT DEFERRABLE 
+)
+;
+
+
+

fb907d3cc213b8b4bf0595d7f32175b411827126
premav7890 <premav7890>
2024-10-04 22:32:16 +0530

    hi

--- Sn1.sql
+++ Sn1.sql
@@ -2,7 +2,7 @@
 -- ER/Studio Data Architect SQL Code Generation
 -- Project :      DATA MODEL
 --
--- Date Created : Friday, October 04, 2024 22:12:20
+-- Date Created : Friday, October 04, 2024 22:32:16
 -- Target DBMS : Snowflake
 --
 
@@ -14,9 +14,7 @@ CREATE TABLE PVSCHEMA."Entity1"(
     "A"  CHAR(10)      NOT NULL,
     "B"  BINARY(10),
     "C"  CHAR(10),
-    CONSTRAINT "PK1" PRIMARY KEY ("A") ENFORCED ,
-    CONSTRAINT "uni"  UNIQUE ("B"),
-    CONSTRAINT "nonpk"  UNIQUE ("C")
+    CONSTRAINT "PK1" PRIMARY KEY ("A") ENFORCED 
 )
 ;
 
